b7d4f7e9de5db169a44976d4796ab0ba0d5cbdb0
Lemy <147449181+OT-Lemy@users.noreply.github.com>
2023-12-07 17:19:56 -0600

    Diseños ૮ ˶ᵔ ᵕ ᵔ˶ ა

---
+++ DiseñosAct_07_12/DiseñosWinapi/DiseñoWinapi.h
@@ -0,0 +1,7 @@
+#include "resource.h"
+
+LRESULT CALLBACK VentanaUsuario(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+LRESULT CALLBACK VentanaRegistro(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+LRESULT CALLBACK VentanaPrincipal(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+bool MenuUsuario(int opcion, HWND ventana);
+
---
+++ DiseñosAct_07_12/DiseñosWinapi/resource.h
@@ -0,0 +1,89 @@
+//{{NO_DEPENDENCIES}}
+// Archivo de inclusión generado de Microsoft Visual C++.
+// Usado por DiseñosWinapi.rc
+//
+#define ID_DIA_Login                    101
+#define ID_DIA_RegistroUss              103
+#define ID_MENU1                        105
+#define ID_DIA_RegistrarCita            106
+#define ID_DIA_Menu                     108
+#define ID_DIA_EditarCita               110
+#define ID_DIA_EliminarCita             112
+#define ID_DIA_CerrarSesion             116
+#define ID_DIA_Agenda                   118
+#define ID_DIAL_NewRegCitas             124
+#define ID_BTN_Acceder                  1002
+#define ID_BTN_RegistrarUss             1003
+#define ID_EC_LoginUss                  1004
+#define ID_EC_LoginPassword             1005
+#define ID_EC_RegNombreDoctor           1006
+#define ID_EC_RegCedula                 1007
+#define ID_EC_RegClaveUss               1008
+#define ID_EC_RegContrasena             1009
+#define ID_BTN_RegCitaOK                1010
+#define ID_BTN_RegCitaCancel            1011
+#define ID_DATE_RegistroFecha           1012
+#define ID_EC_RegCitaNombre             1013
+#define ID_EC_RegCitaTel                1014
+#define ID_EC_RegCitaMascota            1015
+#define ID_EC_RegCitaMotivo             1016
+#define ID_COMB_RegCitaEspecie          1020
+#define ID_EC_RegCitaCosto              1021
+#define ID_BTN_RegExaminar              1023
+#define ID_FOTO_RegUss                  1024
+#define ID_EC_SelCitaAEliminar          1029
+#define ID_BTN_AgendaEditarCita         1030
+#define ID_BTN_AgendaMenu               1031
+#define ID_COMB_CitasAgendadas          1032
+#define ID_COMB_CitasProximas           1033
+#define ID_COMB_CitasCanceladas         1034
+#define ID_BTN_EditarOK                 1035
+#define ID_BTN_Edit_BuscarCita          1036
+#define ID_BTN_EditarCANCEL             1038
+#define ID_LBOX_Editar                  1039
+#define ID_EC_Edit_IngCita              1040
+#define ID_LBOX_CitaEliminar            1042
+#define ID_BTN_BuscarCitaEliminar       1043
+#define ID_EC_EDIT_Costo                1046
+#define ID_COMB_EDIT_Status             1049
+#define ID_EC_EDIT_Motivo               1050
+#define ID_COMB_RegCitaStatus           1051
+#define ID_BTN_OkCerrarSesion           1052
+#define ID_BTN_CancelCerrarSesion       1053
+#define IDC_BUTTON1                     1054
+#define ID_BTN_EliminarOK               1054
+#define ID_BTN_RegUssOK                 1055
+#define ID_BTN_EliminarCANCEL           1056
+#define IDC_LIST1                       1057
+#define ID_LBOX_AgendaMenu              1057
+#define ID_EC_NEWRegCitaNombre          1058
+#define ID_EC_NEWRegCitaTel             1059
+#define ID_EC_NEWRegCitaMascota         1060
+#define ID_EC_NEWRegCitaMotivo          1061
+#define ID_DATE_NEWRegistroFecha        1062
+#define ID_EC_NEWRegCitaCosto           1063
+#define ID_DATE_NEWRegistroHora         1064
+#define ID_COMB_NEWRegCitaStatus        1066
+#define ID_COMB_NEWRegCitaEspecie       1067
+#define IDC_CHECK1                      1068
+#define IDC_CHECK2                      1070
+#define IDC_CHECK3                      1071
+#define ID_AYUDA_SOBRENOSOTROS          40005
+#define ID_MEN_Agenda                   40015
+#define ID_MEN_Anadir                   40016
+#define ID_MEN_Eliminar                 40017
+#define ID_MEN_CerrarSesion             40018
+#define ID_MEN_Salir                    40019
+#define ID_MEN_SobreNosotros            40020
+#define ID_MEN_Editar                   40021
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        126
+#define _APS_NEXT_COMMAND_VALUE         40022
+#define _APS_NEXT_CONTROL_VALUE         1072
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
